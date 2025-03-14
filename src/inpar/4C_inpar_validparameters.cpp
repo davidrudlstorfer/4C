91b070d5981c69484dd9e6a575793eb871b56047
David Rudlstorfer <david.rudlstorfer@tum.de>
2025-03-14 15:47:57 +0100

    Move EHL inpar to physics

--- src/inpar/4C_inpar_validparameters.cpp
+++ src/inpar/4C_inpar_validparameters.cpp
@@ -11,6 +11,7 @@
 #include "4C_beamcontact_input.hpp"
 #include "4C_browniandyn_input.hpp"
 #include "4C_cut_input.hpp"
+#include "4C_ehl_input.hpp"
 #include "4C_inpar_beaminteraction.hpp"
 #include "4C_inpar_beampotential.hpp"
 #include "4C_inpar_binningstrategy.hpp"
@@ -19,7 +20,6 @@
 #include "4C_inpar_cardiovascular0d.hpp"
 #include "4C_inpar_constraint_framework.hpp"
 #include "4C_inpar_contact.hpp"
-#include "4C_inpar_ehl.hpp"
 #include "4C_inpar_elch.hpp"
 #include "4C_inpar_fbi.hpp"
 #include "4C_inpar_fluid.hpp"
@@ -197,7 +197,7 @@ std::map<std::string, Core::IO::InputSpec> Input::valid_parameters()
   Inpar::POROMULTIPHASE::set_valid_parameters(list);
   Inpar::PoroMultiPhaseScaTra::set_valid_parameters(list);
   Inpar::POROFLUIDMULTIPHASE::set_valid_parameters(list);
-  Inpar::EHL::set_valid_parameters(list);
+  EHL::set_valid_parameters(list);
   Inpar::SSI::set_valid_parameters(list);
   Inpar::SSTI::set_valid_parameters(list);
   ALE::set_valid_parameters(list);
