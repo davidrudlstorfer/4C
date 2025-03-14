433888aafc1e153ce912cb424db842ff10ef31ed
David Rudlstorfer <david.rudlstorfer@tum.de>
2025-03-14 15:59:15 +0100

    Move FBI inpar to physics

--- src/inpar/4C_inpar_validparameters.cpp
+++ src/inpar/4C_inpar_validparameters.cpp
@@ -12,6 +12,7 @@
 #include "4C_browniandyn_input.hpp"
 #include "4C_cut_input.hpp"
 #include "4C_ehl_input.hpp"
+#include "4C_fbi_input.hpp"
 #include "4C_inpar_beaminteraction.hpp"
 #include "4C_inpar_beampotential.hpp"
 #include "4C_inpar_binningstrategy.hpp"
@@ -21,7 +22,6 @@
 #include "4C_inpar_constraint_framework.hpp"
 #include "4C_inpar_contact.hpp"
 #include "4C_inpar_elch.hpp"
-#include "4C_inpar_fbi.hpp"
 #include "4C_inpar_fluid.hpp"
 #include "4C_inpar_fpsi.hpp"
 #include "4C_inpar_fs3i.hpp"
@@ -209,7 +209,7 @@ std::map<std::string, Core::IO::InputSpec> Input::valid_parameters()
   Inpar::ReducedLung::set_valid_parameters(list);
   Inpar::Cardiovascular0D::set_valid_parameters(list);
   Inpar::FPSI::set_valid_parameters(list);
-  Inpar::FBI::set_valid_parameters(list);
+  FBI::set_valid_parameters(list);
 
   Inpar::PARTICLE::set_valid_parameters(list);
 
